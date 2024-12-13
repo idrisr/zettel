3fbc5ac660f90d48e6d99cd593d57cba3c9f4519
Idris Raja <idris.raja@gmail.com>
2024-12-13 09:20:49 -0600

    update nixpkgs version

--- flake.nix
+++ flake.nix
@@ -1,13 +1,13 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/23.11";
+    nixpkgs.url = "github:nixos/nixpkgs/24.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = { nixpkgs, flake-utils, ... }:
     let
       system = flake-utils.lib.system.x86_64-linux;
-      pkgs = import nixpkgs { inherit system; };
+      pkgs = nixpkgs.legacyPackages.${system};
       zettel = pkgs.callPackage ./zettel-plugin.nix { };
     in {
       packages.${system} = { default = zettel; };
